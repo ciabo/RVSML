d40be9e7b89529e82e5337b99d7a535db9bbed81
ciabo <lucaciabo@hotmail.it>
2020-08-24 11:48:23 +0200

    Update metricLearning.py

--- metricLearning.py
+++ metricLearning.py
@@ -1,8 +1,10 @@
 import numpy as np
 import dtw
 import OPW
+from scipy.spatial.distance import cdist
 
-def optimize(trainset,templateNum,l,err_limit, align_algorithm = "dtL", lambda1=50, lambda2=12.1, sigma=1):
+def optimize(trainset, templateNum, l, err_limit, align_algorithm="dtw", lambda1=50, lambda2=0.1, sigma=1,
+             previousExercise=None, exercise=None):
     """
     :param trainset: list containing the training sequences divided by class
                         trainset: a list with size of (1, the number of classes);
@@ -11,13 +13,13 @@ def optimize(trainset,templateNum,l,err_limit, align_algorithm = "dtL", lambda1=
     :param templateNum: dimensions of virtual sequences
     :param l: lambda value
     :param err_limit: bottom limit for the error to stop the optimization
-    :param align_algorithm: algorithm used to align sequences "dtL" or "opL"
-    :param lambda1: lambda 1 value used in OPL
-    :param lambda2: lambda 2 value used in OPL
-    :param sigma: sigma value used in OPL
+    :param align_algorithm: algorithm used to align sequences "dtw" or "opw"
+    :param lambda1: lambda 1 value used in OPW
+    :param lambda2: lambda 2 value used in OPW
+    :param sigma: sigma value used in OPW
     :return: the value of L in the decomposizìtion of M in mahalanobis distance
     """
-    #create virtual sequences V
+    # create virtual sequences V
     classNum = len(trainset)
     trainsetnum = np.zeros(shape=(classNum))
     V = []
@@ -30,51 +32,53 @@ def optimize(trainset,templateNum,l,err_limit, align_algorithm = "dtL", lambda1=
         for a in range(templateNum):
             V[c][a][activedim] = 1
             activedim += 1
-    N = sum(trainsetnum)
-    #initialize the alignment matrices T
-    T = []
+    # initialize the alignment matrices T
+    N = np.sum(trainsetnum)
+
+    L_a = np.zeros(shape=(dim, dim))
+    L_b = np.zeros(shape=(dim, downdim))
     for c in range(classNum):
-        tmpT = []
         for n in range(len(trainset[c])):
             seqLen = trainset[c][n].shape[0]
-            tmpT.append(np.ones(shape=(seqLen,templateNum)) / (seqLen * templateNum))
-        T.append(tmpT)
+            T_ini = np.ones(shape=(seqLen,templateNum)) / (seqLen*templateNum)
+            for i in range(seqLen):
+                k = np.expand_dims(trainset[c][n][i], 0)
+                temp_ra = np.transpose(k).dot(k)
+                for j in range(templateNum):
+                    L_a = L_a + T_ini[i][j] * temp_ra
+                    L_b = L_b + T_ini[i][j] * np.transpose(k).dot(np.expand_dims(V[c][j], 0))
+    L_i = L_a + l * N * np.identity(dim)
+    L = np.linalg.solve(L_i, L_b)
 
-    #optimization
+
+    # optimization
     loss_old = 10 ^ 8
     maxIterations = 1000
     for k in range(maxIterations):
+        printProgressBar(k, maxIterations, 'Iterations of '  + ': ' + str(k) + '/' + str(maxIterations))
         loss = 0
-        L_a = np.zeros(shape=(dim,dim))
-        L_b = np.zeros(shape=(dim,downdim))
+        L_a = np.zeros(shape=(dim, dim))
+        L_b = np.zeros(shape=(dim, downdim))
         for c in range(classNum):
             for n in range(len(trainset[c])):
-                for i in range(trainset[c][n].shape[0]-1):
-                    for j in range(V[c].shape[0]-1):
-                        L_a += T[c][n][i][j] * np.expand_dims(trainset[c][n][i],1).dot(np.transpose(np.expand_dims(trainset[c][n][i], 1)))
-                        v = np.transpose(np.expand_dims(V[c][j],1))
-                        L_b +=  T[c][n][i][j] * np.expand_dims(trainset[c][n][i],1).dot(v)
-            #update L
-            L_a = L_a + l * N * np.identity(dim)
-            L = np.linalg.solve(L_a, L_b)  #instead inverting L_a solve the linear system
-            print(L)
-            for n in range(len(trainset[c])):
-                #update T
-                if(align_algorithm == "dtL"):
-                    d,path = dtw.fastdtw(trainset[c][n].dot(L), V[c])
-                    T[c][n] = pathToMat(path, T[c][n])
+                seqLen = trainset[c][n].shape[0]
+                if align_algorithm == 'dtw':
+                    d, T = dtw2(np.transpose(trainset[c][n].dot(L)), np.transpose(V[c]))
                 else:
-                    d, T[c][n] = OPW.opw(trainset[c][n].dot(L), V[c], a=None, b=None, lambda1=lambda1, lambda2=lambda2, sigma=sigma, VERBOSE=0)
+                    d, T = opw(trainset[c][n].dot(L), V[c], a=None, b=None, lambda1=lambda1, lambda2=lambda2, sigma=sigma, VERBOSE=0)
                 loss = loss + d
-            loss = loss / N + np.trace(L.dot(np.transpose(L)))
-            if abs(loss - loss_old) < err_limit:
-                break
-            else:
-                loss_old = loss
-    return L
+                for i in range(seqLen):
+                    k = np.expand_dims(trainset[c][n][i], 0)
+                    temp_ra = np.transpose(k).dot(k)
+                    for j in range(templateNum):
+                        L_a = L_a + T[i][j] * temp_ra
+                        L_b = L_b + T[i][j] * np.transpose(k).dot(np.expand_dims(V[c][j], 0))
 
-def pathToMat(path, T):
-    mat = np.zeros(shape=(T.shape[0], T.shape[1]))
-    for el in path:
-        mat[el[0], el[1]] = 1
-    return mat
+        loss = loss / N + np.trace(np.transpose(L).dot(L))
+        if abs(loss - loss_old) < err_limit:
+            break
+        else:
+            loss_old = loss
+        L_i = L_a + l * N * np.identity(dim)
+        L = np.linalg.solve(L_i, L_b)
+    return L
